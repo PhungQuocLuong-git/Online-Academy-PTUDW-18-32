236b4910254b84b6451631522521b74a065caa6e
Quoc Luong <quocluong2503@gmail.com>
2020-12-05 20:06:54 +0700

    them controller

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var SiteCotroller = require('../app/controller/SiteCotroller');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home');
-});
+router.get('/', SiteCotroller.home);
 
 
 
