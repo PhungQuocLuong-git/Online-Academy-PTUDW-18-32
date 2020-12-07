f6f980fc1a3cd6c418f6ba062b9600e1c1f4aed2
Quoc Luong <quocluong2503@gmail.com>
2020-12-07 20:28:41 +0700

    .

--- app.js
+++ app.js
@@ -1,46 +1,30 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const session = require('express-session');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
 
 
 const exphbs = require('express-handlebars');
 
 var app = express();
 
-const db = require('./config/db');
-
-// Connect to DB
-db.connect();
-
-// view engine setup
 app.engine('hbs', exphbs({
+  defaultLayout: 'main.hbs',
   extname: '.hbs'
 }));
+
+
+
+
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
-
-// Session
-app.use(
-  session({
-    resave: false, // don't save session if unmodified
-    saveUninitialized: false, // don't create session until something stored
-    secret: "Dam Linh",
-  })
-);
-
-
-// Routes
-// const usersRouter = require('./routes/users');
-const siteRouter = require('./routes/site');
-// const meRouter = require('./routes/me');
-const accountRouter = require('./routes/account');
-// const coursesRouter = require('./routes/courses');
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -48,9 +32,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', siteRouter);
-// app.use('/home', siteRouter);
-app.use('/account', accountRouter);
+app.use('/', indexRouter);
+app.use('/home', homeRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
