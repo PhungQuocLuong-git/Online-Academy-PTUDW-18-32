ea0a5ac66dbc059946fe42f99434a297b5305ce6
dohoanglong <49149829+dohoanglong@users.noreply.github.com>
2020-12-12 16:55:30 +0700

    Sua loi o package.json de deploy

--- app.js
+++ app.js
@@ -6,12 +6,25 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
+
+
+const exphbs = require('express-handlebars');
 
 var app = express();
 
+app.engine('hbs', exphbs({
+  defaultLayout: 'main.hbs',
+  extname: '.hbs'
+}));
+
+
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +32,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
+app.use('/home', homeRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +53,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// app.get('/home', (req, res) => {
+//   res.render('home');
+
+// })
 
 module.exports = app;
