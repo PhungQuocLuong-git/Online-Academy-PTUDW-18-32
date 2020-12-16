7dfbffbeeb32190116f61722b1340c230ce63875
Tấn Phát Tô Nguyễn <tonguyentanphat@gmail.com>
2020-12-16 22:50:51 +0700

    Them chuc nang chuyen doi role Student va teacher o header

--- routes/account.js
+++ routes/account.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 const accountController = require('../app/controller/AccountController');
 
+router.patch('/:id/swap', accountController.swap );
 router.get('/create', accountController.create);
 router.post('/store', accountController.store);
 router.get('/login', accountController.login);
 router.post('/check', accountController.check);
 router.post('/login', accountController.create);
+router.get('/logout', accountController.logout);
 
 module.exports = router;
