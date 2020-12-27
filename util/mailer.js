cd58851f5565c88ffa49d4a4bf0a05bec313b2ec
Tấn Phát Tô Nguyễn <tonguyentanphat@gmail.com>
2020-12-27 15:10:20 +0700

    Sua util mailer

--- util/mailer.js
+++ util/mailer.js
@@ -3,24 +3,24 @@ const adminEmail = 'nhom15webhcmus@gmail.com'
 const adminPassword = 'adminAdm1n'
 const mailHost = 'smtp.gmail.com'
 const mailPort = 587
-const sendMail = (to, subject, htmlContent) => {
-  const transporter = nodeMailer.createTransport({
-    host: mailHost,
-    port: mailPort,
-    secure: false,
-    auth: {
-      user: adminEmail,
-      pass: adminPassword
-    }
-  })
-  const options = {
-    from: adminEmail, 
-    to: to, 
-    subject: subject, 
-    html: htmlContent 
-  }
-  return transporter.sendMail(options)
-}
 module.exports = {
-  sendMail: sendMail
+  sendMail(to, subject, htmlContent){
+    const transporter = nodeMailer.createTransport({
+      host: mailHost,
+      port: mailPort,
+      secure: false,
+      auth: {
+        user: adminEmail,
+        pass: adminPassword
+      }
+    })
+    const options = {
+      from: `"Hacker 🐧 " <${adminEmail}>`, 
+      to: to, 
+      subject: subject, 
+      html: htmlContent 
+    }
+    return transporter.sendMail(options)
+  }
+  
 }
