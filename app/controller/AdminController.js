84d57f95840b3c29865210e912e0437ee3bb45ea
Quoc Luong <quocluong2503@gmail.com>
2021-01-02 11:14:55 +0700

    fix xoa course voi cac ref

--- app/controller/AdminController.js
+++ app/controller/AdminController.js
@@ -72,7 +72,7 @@ class AdminController {
     trash(req,res) {
         Course.find({status:-1})
             .then(courses => {
-                console.log(courses)
+                // console.log(courses)
                 res.render('admin/courses',{
                     layout:'admin',
                     courses:multipleMongooseToObject(courses),
@@ -112,7 +112,9 @@ class AdminController {
         res.render('admin/categories', {
             empty: list.length === 0,
             categories: list,
-            layout:'admin'
+            layout:'admin',
+            extraStyle:'/public/stylesheets/home.css',
+
         });
 
     };
