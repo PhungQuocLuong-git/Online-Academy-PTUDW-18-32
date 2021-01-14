44d976ddb53bc827c9c3d10ef26df6e87ce35daf
Quoc Luong <quocluong2503@gmail.com>
2021-01-14 14:32:13 +0700

    thay doi db de deploy

--- config/db/index.js
+++ config/db/index.js
@@ -11,7 +11,7 @@ async function connect() {
         console.log('Sucessfully Connected !!!')
     } */
     try {
-        await mongoose.connect('mongodb+srv://dbUser2:dbUser2@cluster0.krhp6.mongodb.net/doanweb?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://dbUser:dbUser@cluster0.edbz1.mongodb.net/doanweb?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
